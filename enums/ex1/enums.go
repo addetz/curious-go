12479bedbd6c100d20486365520ee0288f5a8bcd
addetz <adelinasimion@gmail.com>
2024-05-01 19:03:29 +0100

    add comment

--- enums/ex1/enums.go
+++ enums/ex1/enums.go
@@ -2,6 +2,13 @@ package enums
 
 type BookCategory int
 
+// Novel: 1
+// Fairytale: 2
+// Drama: 3
+// Poetry: 4
+// Technical: 5
+// Biography: 6
+
 const (
 	Novel BookCategory = iota
 	FairyTale
